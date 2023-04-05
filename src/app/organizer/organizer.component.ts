0526f639e68507713036abf9a3d20a8fb2053be2
Эмиль <vagab@>
2023-04-05 17:15:13 +0300

    completed

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {DateService} from "../shared/date.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Task, TasksService} from "../shared/tasks.service";
+import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-organizer',
@@ -10,10 +11,16 @@ import {Task, TasksService} from "../shared/tasks.service";
 })
 export class OrganizerComponent implements OnInit{
   form:FormGroup
+  tasks:Task[] = []
   constructor(public dateService:DateService,private taskService:TasksService) {
   }
 
   ngOnInit() {
+    this.dateService.date.pipe(
+      switchMap(value => this.taskService.load(value))
+    ).subscribe(tasks =>{
+      this.tasks = tasks
+    })
     this.form = new FormGroup({
       title:new FormControl("",Validators.required)
     })
@@ -26,9 +33,15 @@ export class OrganizerComponent implements OnInit{
       date: this.dateService.date.value.format("DD-MM-YYYY")
     }
     this.taskService.create(task).subscribe(task =>{
+      this.tasks.push(task)
       this.form.reset()
-
     },error => console.log(error)
     )
+  }
+
+  remove(task: Task) {
+    this.taskService.remove(task).subscribe(()=>{
+      this.tasks = this.tasks.filter(t=>t.id != task.id)
+    },error => console.error(error))
   }
 }
--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
+import * as moment from "moment";
 export interface Task {
   title:string
   id?:string
@@ -17,11 +18,25 @@ export class TasksService {
   create(task:Task):Observable<Task>{
     return this.http.post<CreateResp>(`${TasksService.url}/${task.date}.json`,task).
     pipe(map(res => {
-      console.log("resp",res)
       return {
         ...task,
         id:res.name
       }
     }))
+  }
+  load(date:moment.Moment):Observable<Task[]>{
+    return this.http.get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YYYY')}.json`)
+      .pipe(map(tasks =>{
+        if(!tasks){
+          return []
+        }
+        return Object.keys(tasks).map(key =>({
+          ...tasks[key],
+          id:key
+        }))
+      }))
+  }
+  remove(task:Task):Observable<void>{
+    return this.http.delete<void>(`${TasksService.url}/${task.date}/${task.id}.json`)
   }
 }
